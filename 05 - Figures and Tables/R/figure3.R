5d58bb882eb28fd0fd9a48a21b434295c60a4975
kkrupins <kkrupins@umich.edu>
2026-02-16 14:44:47 -0500

    Add in ability to filter out one pair

--- 05 - Figures and Tables/R/figure3.R
+++ 05 - Figures and Tables/R/figure3.R
@@ -12,10 +12,15 @@ source("./01 - Functions/calculateBottleneckByLevels.R")
 source("./01 - Functions/calculateOverallBottleneck.R")
 source("./01 - Functions/formatClonalDataForPlotting.R")
 source("./01 - Functions/makePairTable.R")
-source(
-  "./01 - Functions/plotBottleneckSizeByMetadataBars_bothMethods.R"
-)
+source("./01 - Functions/plotBottleneckSizeByMetadataBars_bothMethods.R")
 source("./01 - Functions/findCI.R")
+
+noFive <- TRUE
+if (noFive) {
+  pathToClonalOutput <- "./04 - Output/Clonal_data/clonal_mut_all_output_noFive.csv"
+} else {
+  pathToClonalOutput <- "./04 - Output/Clonal_data/clonal_mut_all_output.csv"
+}
 
 # 1. Format data ---------------------------------------------------------------
 nonzero_pairs <- read.table("./04 - Output/iSNV_data/01 - Output/num_vars.txt", header = TRUE) %>%
@@ -29,14 +34,20 @@ isnv_pair <- read.table("./04 - Output/iSNV_data/pair_meta.txt", header = TRUE) 
 clonal_pair <- read.table("./04 - Output/Clonal_data/clonal_dist_with_meta.txt",
                           header = TRUE)
 
+if (noFive == TRUE) {
+  isnv_pair <- isnv_pair %>% filter(donor_id != 1809801502 & recipient_id != 1809802501)
+  clonal_pair <- clonal_pair %>% filter(donor_id != 1809801502 & recipient_id != 1809802501)
+}
+
 isnvData <- calculateBottleneckByMetadata(
   pathToConfidenceInterval = './04 - Output/iSNV_data/01 - Output/confidence_int.txt',
   pathToLogLikelihood = './04 - Output/iSNV_data/01 - Output/logLikelihood.txt',
   pathToNumVars = './04 - Output/iSNV_data/01 - Output/num_vars.txt',
-  pathToPairMeta = "./04 - Output//iSNV_data/pair_meta.txt"
+  pathToPairMeta = "./04 - Output//iSNV_data/pair_meta.txt",
+  noFive = noFive
 )
 
-clonalData <- formatClonalDataForPlotting(pathToOutput = "./04 - Output/Clonal_data/clonal_mut_all_new.csv",
+clonalData <- formatClonalDataForPlotting(pathToOutput = pathToClonalOutput,
                                           isnv_pair = isnv_pair,
                                           clonal_pair = clonal_pair) %>%
   na.omit()
@@ -48,12 +59,21 @@ p <- plotBottleneckSizeByMetadataBars_bothMethods(isnvData = isnvData, clonalDat
 save <- TRUE
 
 if (save) {
-  ggsave(
-    filename = './05 - Figures and Tables/Rendered/fig3.tif',
-    plot = p,
-    width = 15,
-    height = 8
-  )
+  if (noFive) {
+    ggsave(
+      filename = './05 - Figures and Tables/Rendered/fig3_noFive.png',
+      plot = p,
+      width = 15,
+      height = 8
+    )
+  } else {
+    ggsave(
+      filename = './05 - Figures and Tables/Rendered/fig3.tif',
+      plot = p,
+      width = 15,
+      height = 8
+    )
+  }
 }
 
 rm(list = setdiff(ls(), c()))
